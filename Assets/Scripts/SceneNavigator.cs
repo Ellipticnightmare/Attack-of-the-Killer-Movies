fc8704c36b52fa8f8da5b4983584bdc5250a692c
Abias24 <53461223+Abias24@users.noreply.github.com>
2022-04-04 14:36:28 +1000

    created scenenavigator script to move between scenes

---
+++ Assets/Scripts/SceneNavigator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour
+{
+    public int minutes = 0;
+    public int seconds = 0;
+
+    public string highscore = "0:00:00";
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+}
