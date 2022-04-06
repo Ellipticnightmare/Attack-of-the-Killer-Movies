0ad7b0104233b7fd2ba300118902bfaffcca64d5
Ellipticnightmare <31974144+Ellipticnightmare@users.noreply.github.com>
2022-04-06 20:32:08 +1000

    Update MonsterController.cs

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -12,7 +12,7 @@ public class MonsterController : MonoBehaviour
     public bool hasAnimations;
     NavMeshAgent agent;
     NavMeshPath path;
-    List<webNodePoint> nodeWeb;
+    List<webNodePoint> nodeWeb = new List<webNodePoint>();
     webNodePoint curNode, nextNode;
     Animator anim;
     public int nodeMinDistance, nodeWeightDistance;
@@ -39,7 +39,7 @@ public class MonsterController : MonoBehaviour
         agent = this.GetComponent<NavMeshAgent>();
         anim = this.GetComponent<Animator>();
         path = new NavMeshPath();
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraHandler>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponentInParent<CameraHandler>();
         BuildNode();
     }
 
