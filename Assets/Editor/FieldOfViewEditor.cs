94703df6804aac67ae2aa0e6acc7058ee77c0105
Ellipticnightmare <31974144+Ellipticnightmare@users.noreply.github.com>
2022-05-05 12:35:43 +1000

    fixed merge changes
    all fixes from previous merge, and created SFX manager as well

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -9,12 +9,12 @@ public class FieldOfViewEditor : Editor
     {
         MonsterController fow = (MonsterController)target;
         Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.MyAIManager.viewConeRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.MyAIManager.viewConeAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.MyAIManager.viewConeAngle / 2, false);
 
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.MyAIManager.viewConeRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.MyAIManager.viewConeRadius);
 
         Handles.color = Color.red;
     }
